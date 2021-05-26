edb0c72312e15767ccf94cf5dadfd62d467db291
hp <hp@DESKTOP-LP5U1KO>
2021-05-26 17:38:06 +0200

    sol1

--- src/main/java/it/polito/tdp/artsmia/model/Model.java
+++ src/main/java/it/polito/tdp/artsmia/model/Model.java
@@ -1,5 +1,53 @@
 package it.polito.tdp.artsmia.model;
 
-public class Model {
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-}
+import org.jgrapht.Graph;
+import org.jgrapht.Graphs;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.SimpleWeightedGraph;
+
+import it.polito.tdp.artsmia.db.ArtsmiaDAO;
+
+public class Model {
+private Graph<ArtObject,DefaultWeightedEdge>grafo;
+private ArtsmiaDAO dao;
+private Map<Integer,ArtObject>idMap;
+	public Model() {
+	 //NON CREO IL GRAFO NEL MODEL MA NEL CREA GRAFO
+	  // grafo=new SimpleWeightedGraph<>(DefaultWeightedEdge.class);
+		dao= new ArtsmiaDAO();
+		idMap=new HashMap<Integer,ArtObject>();
+	}
+	
+	public void creaGrafo() {
+		
+		grafo=new SimpleWeightedGraph<>(DefaultWeightedEdge.class);
+		//AGGIUNGO I VERTICI
+		//RECUPERO TUTTI GLI ARTOBJECT DAL DB E LI INSERISCO COME I VERTICI
+		dao.listObjects(idMap) ;
+		Graphs.addAllVertices(grafo, idMap.values());
+		//AGGIUNGERE ARCHI
+		//APPROCCIO 1 
+		//DOPPIO CICLO FOR SUI VERTICI,DATI DUE VERTICI CONTROLLO SE DEVONO ESSERE COLLEGATI
+		
+		/*for(ArtObject a1:this.grafo.vertexSet()) {
+			for(ArtObject a2:this.grafo.vertexSet()) {
+			if(!a1.equals(a2) && !this.grafo.containsEdge(a1,a2))//CONTROLLO SE I DUE ELEMENTI NON SONO UGUALIE SE NON C'è GIA UN ARCO 
+			{
+				 int peso= this.dao.getPeso(a1,a2);
+				 if(peso>0)
+					 Graphs.addEdge(this.grafo, a1, a2, peso);
+			}	
+		    }*/
+		
+		//APPROCCIO 3 
+		for(Adiacenza a :dao.getAdiacenze()) {
+				Graphs.addEdge(this.grafo, idMap.get(a.getId1()), idMap.get(a.getId2()), a.getPeso());
+			
+		}
+		}
+	}
+
